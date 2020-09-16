a75a49cd2fd4edb91025afe1c6abf7e9e351af8b
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2020-09-16 16:20:24 +0300

    Feat: Improve ligato-vpp interface names. (#25)
    * Feat: Improve ligato-vpp interface names.
    * Added peerName label into reusable constant

--- pkg/universal-cnf/vppagent/backend.go
+++ pkg/universal-cnf/vppagent/backend.go
@@ -23,6 +23,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/cisco-app-networking/nsm-nse/pkg/universal-cnf/config"
 	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connection"
 	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connection/mechanisms/memif"
 	"github.com/sirupsen/logrus"
@@ -98,6 +99,23 @@ func (b *UniversalCNFVPPAgentBackend) ProcessClient(
 	return nil
 }
 
+func (b *UniversalCNFVPPAgentBackend) buildVppIfName(defaultIfName, serviceName string, conn *connection.Connection) string {
+	// NSC peer connection
+	if name, ok := conn.Labels[connection.PodNameKey]; ok {
+		logrus.Infof("Setting ifName with podName: %s", name)
+		return name
+	}
+
+	// vl3 NSE peer connection
+	if name, ok := conn.Labels[config.PEER_NAME]; ok {
+		logrus.Infof("Setting ifName with vL3 peer name: %s", name)
+		return name
+	}
+
+	// create a default interface name, using the prefix and the generated id
+	return defaultIfName + b.GetEndpointIfID(serviceName)
+}
+
 // ProcessEndpoint runs the endpoint code for VPP CNF
 func (b *UniversalCNFVPPAgentBackend) ProcessEndpoint(
 	dpconfig interface{}, serviceName, ifName string, conn *connection.Connection) error {
@@ -115,7 +133,8 @@ func (b *UniversalCNFVPPAgentBackend) ProcessEndpoint(
 		ipAddresses = append(ipAddresses, dstIP)
 	}
 
-	endpointIfName := ifName + b.GetEndpointIfID(serviceName)
+	endpointIfName := b.buildVppIfName(ifName, serviceName, conn)
+
 	rxModes := []*interfaces.Interface_RxMode{
 		&interfaces.Interface_RxMode{
 			Mode:        interfaces.Interface_RxMode_INTERRUPT,
