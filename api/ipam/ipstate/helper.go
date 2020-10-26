79dcd16d86e9382727ee870281fa316ef9e45849
AnaMariaMoga <AMoga@luxoft.com>
2020-10-26 14:43:28 +0200

    Delete validationErrors

--- api/ipam/ipstate/helper.go
+++ api/ipam/ipstate/helper.go
@@ -1,32 +1,13 @@
 package ipstate
 
-import (
-	"bytes"
-	"fmt"
-)
-
-type validationErrors []error
-
-func (e validationErrors) Error() string {
-	b := bytes.NewBufferString("")
-	for _, err := range e {
-		_, _ = fmt.Fprintf(b, "\t%s", err)
-	}
-	return b.String()
-}
-
 func (m *PrefixIdentifier) Validate() error {
-	var errs validationErrors
 	err := m.GetIdentifier().Validate()
 	if err != nil {
-		errs = append(errs, err)
+		return err
 	}
 	err = m.GetAddrFamily().Validate()
 	if err != nil {
-		errs = append(errs, err)
-	}
-	if len(errs) > 0 {
-		return errs
+		return err
 	}
 	return nil
 }
