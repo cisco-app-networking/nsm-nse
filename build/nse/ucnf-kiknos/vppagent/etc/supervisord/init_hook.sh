bdaffcb04d98b745074086565073a0cc6aad076f
Rastislav Szabo <raszabo@cisco.com>
2020-10-27 16:40:20 +0100

    Add proper Kiknos supervisor config + init hook
    Signed-off-by: Rastislav Szabo <raszabo@cisco.com>

---
+++ build/nse/ucnf-kiknos/vppagent/etc/supervisord/init_hook.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+terminate_process () {
+    PID=$(pidof $1)
+    if [[ ${PID} != "" ]]; then
+        kill ${PID}
+        echo "process $1 terminated"
+    fi
+}
+
+if [[ "${SUPERVISOR_PROCESS_NAME}" = "vpp" && "${SUPERVISOR_PROCESS_STATE}" = "terminated" ]]; then
+    terminate_process vpp-agent-init
+fi
+
+if [[ "${SUPERVISOR_PROCESS_NAME}" = "aio-agent" && "${SUPERVISOR_PROCESS_STATE}" = "terminated" ]]; then
+    terminate_process vpp-agent-init
+fi
+
+if [[ "${SUPERVISOR_PROCESS_NAME}" = "strongswan" && "${SUPERVISOR_PROCESS_STATE}" = "terminated" ]]; then
+    terminate_process vpp-agent-init
+fi
+
+if [[ "${SUPERVISOR_PROCESS_NAME}" = "cnf" && "${SUPERVISOR_PROCESS_STATE}" = "terminated" ]]; then
+    terminate_process vpp-agent-init
+fi
